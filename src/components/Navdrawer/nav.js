7886ae22deb6bec9dedda5ed28995aa44c76356d
jawnwa22 <jawnwa22@gmail.com>
2019-07-31 20:20:47 +0800

    chore：新增导航栏配置文件

---
+++ src/components/Navdrawer/nav.js
@@ -0,0 +1,19 @@
+let nav = [
+    {
+        title: 'dashboard',
+        icon: 'dashboard',
+        to: '/dashboard'
+    },
+    {
+        title: '文章管理',
+        icon: 'view_list',
+        to: '/article_list'
+    },
+    {
+        title: '类别管理',
+        icon: 'label',
+        to: '/category_list'
+    }
+]
+
+export default nav;
