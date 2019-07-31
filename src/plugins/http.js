569905c79cb3c1ae2cfc2c08223ac218db58efe9
jawnwa22 <jawnwa22@gmail.com>
2019-07-31 20:20:03 +0800

    chore：新增axios框架

---
+++ src/plugins/http.js
@@ -0,0 +1,61 @@
+/**
+ * Created Date: 2019-06-11
+ * Author: jawnwa22
+ * @description 导出axios对象
+ * @use
+ *
+ * Copyright (c) 2019 19cm_without_head Team
+ */
+
+import axios from "axios";
+import store from "@/store";
+
+axios.defaults.headers.post["Content-Type"] =
+    "application/x-www-form-urlencoded";
+//用来处理刷新token后重新请求的自定义变量
+axios.defaults.isRetryRequest = false;
+
+let config = {
+    baseURL: process.env.VUE_APP_BASEURL || process.env.apiUrl || "",
+    timeout: 60 * 1000 // Timeout
+};
+
+const _axios = axios.create(config);
+
+_axios.interceptors.request.use(
+    function(config) {
+        //添加Authorization 字段
+        let token = window.localStorage.getItem("token");
+        config.headers.Authorization = "Bearer " + token;
+
+        return config;
+    },
+    function(error) {
+        // Do something with request error
+        store.commit("closeLoading");
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+_axios.interceptors.response.use(
+    function(response) {
+        // Do something with response data
+        let code = response.data.code;
+        // success为请求成功的响应码
+        if (code === "success") {
+            if (response.data.data) {
+                return response.data.data;
+            }
+            return response.data;
+        }
+        return response;
+    },
+    function(error) {
+        // Do something with response error
+        store.commit("closeLoading");
+        return Promise.reject(error);
+    }
+);
+
+export default _axios;
