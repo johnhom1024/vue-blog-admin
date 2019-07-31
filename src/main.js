68644c84da5ceef110a78b68230ec0a339fa2add
jawnwa22 <jawnwa22@gmail.com>
2019-07-31 20:21:48 +0800

    chore：导入API接口以及vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import '@/api/index';   // 导入封装的api
+import '@/assets/style/common.scss';
+import store from './store'
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app");
