67221fc01cbb147616e1329e1a3b25732b0ef758
jawnwa22 <jawnwa22@gmail.com>
2019-08-01 09:20:41 +0800

    feat(vue.config.js)：新增打包插件配置
    - webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,25 @@
-const chainWebpack = config => {
-    config
-        .plugin("webpack-bundle-analyzer")
-        .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin);
-};
+/**
+ * @description: vue配置文件
+ * @since: 2019-07-31 20:23:23
+ * @Author: jawnwa22
+ * @LastEditors: jawnwa22
+ * @LastEditTime: 2019-07-31 23:26:09
+ */
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     publicPath: "/admin/",
-    chainWebpack,
     devServer: {
         historyApiFallback: {
-            rewrites: [
-                { from: /^.*/, to: '/admin/index.html'}
-            ]
+            rewrites: [{ from: /^.*/, to: "/admin/index.html" }]
         }
+    },
+    configureWebpack: {
+        plugins: [
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false
+            })
+        ]
     }
 };
