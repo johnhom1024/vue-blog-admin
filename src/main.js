1c1534e0bf0aa75baa3890e5dde068fcf229c645
jawnwa22 <jawnwa22@gmail.com>
2019-08-04 15:50:22 +0800

    update(vuetify)：更新vuetify到2.X版本

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import Vue from "vue";
-import "./plugins/vuetify";
+
 import App from "./App.vue";
 import router from "./router";
 
 import '@/api/index';   // 导入封装的api
 import '@/assets/style/common.scss';
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App)
 }).$mount("#app");
