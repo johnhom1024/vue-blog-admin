3dd85ca3ce4c70885aa64494edfd87d6f9b2f6c3
jawnwa22 <jawnwa22@gmail.com>
2019-12-20 19:45:05 +0800

    chore(配置文件)：更新配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,16 @@
-/**
- * @description: vue配置文件
- * @since: 2019-07-31 20:23:23
- * @Author: jawnwa22
- * @LastEditors: jawnwa22
- * @LastEditTime: 2019-07-31 23:26:09
- */
-
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
 module.exports = {
-    publicPath: "/admin/",
-    devServer: {
-        historyApiFallback: {
-            rewrites: [{ from: /^.*/, to: "/admin/index.html" }]
+  "publicPath": "/admin/",
+  "devServer": {
+    "historyApiFallback": {
+      "rewrites": [
+        {
+          "from": {},
+          "to": "/admin/index.html"
         }
-    },
-    configureWebpack: {
-        plugins: [
-            new BundleAnalyzerPlugin({
-                openAnalyzer: false
-            })
-        ]
+      ]
     }
-};
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
